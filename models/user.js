9a379b4ed49eb051d50a583ae7298e991d6cefbb
kirti230 <kirtivishwakarma822@gami.com>
2025-05-18 13:39:23 +0530

    Add Project Files

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const passportLocalMessage = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+
+});
+
+userSchema.plugin(passportLocalMessage);
+
+module.exports= mongoose.model("User",userSchema);
